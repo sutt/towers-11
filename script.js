792caf334ff8a8f4017d4dddaf6873807c1edc87
alfranc <alexfranco12@gmail.com>
2021-04-08 11:55:11 -0500

    reviewed a lesson on modul to add in the rules for the game

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 console.log("script is working");
+
+const openButton = document.getElementById('open-modal');
+const modal = document.getElementById('modal');
+const closeButton = document.getElementById('close-modal');
+
+const openModal = () => {
+    modal.style.display = "block";
+}
+const closeModal = () => {
+    modal.style.display = "none";
+}
+
+// open the "about game" modal by clicking the button
+openButton.addEventListener('click', openModal);
+
+// close the modal
+closeButton.addEventListener('click', closeModal);
