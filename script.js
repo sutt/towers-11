cbb58b82ade72488ddeafc259520e9b359744ee2
alfranc <alexfranco12@gmail.com>
2021-04-09 11:26:00 -0500

    added in drag and drop functions and made sure the user can only drag the top most disk

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-console.log("script is working");
+console.log("script is working! Let's play some Towers");
 
 let disk = null;
+let numOfMoves = document.querySelector(".score").innerHTML;
 
+function addToScore () {
+    document.querySelector(".score").innerHTML = numOfMoves++;
+}
 // event listener for when towers are clicked
 const towers = document.getElementsByClassName("tower");
 for (tower of towers) {
@@ -10,13 +14,43 @@ for (tower of towers) {
             disk = tower.target.firstElementChild;
         } else {
             tower.target.prepend(disk);
+            addToScore();
             disk = null;
         }
     })
 }
 
-// event listener for when disk is dragged
+// a function that makes each of the disks draggable
+function disksDraggable () {
+    for (tower of towers) {
+        // set the disk's draggability to false if it is not on top
+        if (tower.hasChildNodes) {
+            for (let i=0; i<tower.childElementCount; i++){
+                tower.children[i].setAttribute("draggable", "false")
+            }
+        }
+        // tower.children.setAttribute("draggable", "false")
+        if (tower.firstElementChild != null) tower.firstElementChild.setAttribute("draggable", "true");
+    }
+}
+disksDraggable();
 
+// functions for when the disk is dragged
+function allowDrop(event) {
+    event.preventDefault();
+}
+  
+function drag(event) {
+    event.dataTransfer.setData("text", event.target.id);
+}
+  
+function drop(event) {
+    event.preventDefault();
+    let data = event.dataTransfer.getData("text");
+    event.target.prepend(document.getElementById(data));
+    addToScore();
+    disksDraggable();
+}
 
 // about the game modal
 // open the "learn to play" modal by clicking the button
